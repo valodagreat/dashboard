8ae144e86bfecb0ea2e23437e4c43ac904a3704f
caleb1-valo <65986150+caleb1-valo@users.noreply.github.com>
2020-08-02 18:51:19 +0100

    Updates

---
+++ precache-manifest.9b0e1e46ec09dc348222b5e88eeed712.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dea377e5958c3b19486912e8b70cad94",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "beca6c3798a79be22835",
+    "url": "/dashboard/static/css/main.a52aaa4e.chunk.css"
+  },
+  {
+    "revision": "52954c2d50c51f840005",
+    "url": "/dashboard/static/js/2.6466eef9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dashboard/static/js/2.6466eef9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beca6c3798a79be22835",
+    "url": "/dashboard/static/js/main.8f6fe950.chunk.js"
+  },
+  {
+    "revision": "8f3ec870dce268db1d96",
+    "url": "/dashboard/static/js/runtime-main.2ec0fa51.js"
+  }
+]);
